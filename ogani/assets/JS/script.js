3add442a929b64fdee22bd8010758f9fb3ab9b1b
Arif <147068892+ArifAlibalayev@users.noreply.github.com>
2023-11-17 19:26:19 +0400

    Add files via upload

---
+++ ogani/assets/JS/script.js
@@ -0,0 +1,9 @@
+const  departmensBtn = document.querySelector(".departments-btn")
+const dropdown = document.querySelector(".dropdown-departments")
+
+departmensBtn.addEventListener("click", function () {
+    
+    dropdown.classList.toggle("active")
+})
+
+
